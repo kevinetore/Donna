e2be1c6792390ee39fa87186d61e796b69415728
Kevin Etore <info@etore.nl>
2017-07-25 15:25:36 +0200

    Add multiple thread support for LED's and dejavu. Also implemented Knight Rider style in 5mm LED's

--- donna/donna.py
+++ donna/donna.py
@@ -1,46 +1,58 @@
 import warnings
 import json
 import RPi.GPIO as GPIO
+import time as time
+import threading
+from threading import Thread
 warnings.filterwarnings("ignore")
 
 from dejavu import Dejavu
 from dejavu.recognize import FileRecognizer, MicrophoneRecognizer
 
 GPIO.setmode(GPIO.BOARD)
+pins = [3,5,8,15,16] # In correct order of GPIO on Raspberry Pi
+second_pins = [7,11,13,10,12]
 
-pins = 8, 10
-GPIO.setup(pins, GPIO.OUT)
+board_pins = zip(pins, second_pins)
+print(board_pins)
+
+GPIO.setup(pins+second_pins, GPIO.OUT)
 
 # load config from a JSON file (or anything outputting a python dictionary)
 with open("database.cnf") as f:
     config = json.load(f)
+    
+def piBoard():
+    for pin in board_pins * 15:
+        GPIO.output(
+            pin[0], GPIO.HIGH
+        )
+        GPIO.output(
+            pin[1], GPIO.HIGH
+        )
+        time.sleep(0.15)
+        GPIO.output(
+            pin[0], GPIO.LOW
+        )
+        GPIO.output(
+            pin[1], GPIO.LOW
+        )
+        
+def recognizeSong():
+    # create a Dejavu instance
+    djv = Dejavu(config)
+
+    # Fingerprint mp3's in mp3 directory
+    djv.fingerprint_directory("mp3", [".mp3"])
+    secs = 5
+    song = djv.recognize(MicrophoneRecognizer, seconds=secs)
+    
+    if song is None:
+        print "Nothing recognized -- did you play the song out loud so your mic could hear it? :)"
+    else:
+	print "From mic with %d seconds we recognized: %s\n" % (secs, song)
 
 if __name__ == '__main__':
 
-	# create a Dejavu instance
-	djv = Dejavu(config)
-
-	# Fingerprint all the mp3's in the directory we give it
-	djv.fingerprint_directory("mp3", [".mp3"])
-        
-	# Recognize audio from a file
-	# song = djv.recognize(FileRecognizer, "mp3/SampleAudio_0.4mb.mp3")
-	# print "From file we recognized: %s\n" % song
-
-	# Or recognize audio from your microphone for `secs` seconds
-	GPIO.output(pins, GPIO.HIGH)
-	secs = 5
-	song = djv.recognize(MicrophoneRecognizer, seconds=secs)
-	
-	if song is None:
-		print "Nothing recognized -- did you play the song out loud so your mic could hear it? :)"
-		GPIO.output(pins, GPIO.LOW)
-	else:
-		print "From mic with %d seconds we recognized: %s\n" % (secs, song)
-		GPIO.output(pins, GPIO.LOW)
-        GPIO.cleanup()
-
-	# Or use a recognizer without the shortcut, in anyway you would like
-	# recognizer = FileRecognizer(djv)
-	# song = recognizer.recognize_file("mp3/SampleAudio_0.4mb.mp3")
-	# print "No shortcut, we recognized: %s\n" % song
+    Thread(target = piBoard).start()
+    Thread(target = recognizeSong).start()    
